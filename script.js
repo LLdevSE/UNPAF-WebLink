cb713a3163e45b550fb0395eedcca79225fca470
LLdevSE <lahirufirst1@gmail.com>
2025-12-20 01:33:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const startButton = document.getElementById('startButton');
+const countdownDiv = document.getElementById('countdown');
+const welcomeMessageDiv = document.getElementById('welcomeMessage');
+const title = document.getElementById('title');
+const fireworksContainer = document.getElementById('fireworks-container');
+const fireworks = new Fireworks.default(fireworksContainer);
+
+startButton.addEventListener('click', () => {
+    startButton.classList.add('d-none');
+    title.classList.add('d-none');
+    let count = 5;
+    countdownDiv.textContent = count;
+
+    const interval = setInterval(() => {
+        count--;
+        if (count > 0) {
+            countdownDiv.textContent = count;
+        } else {
+            clearInterval(interval);
+            countdownDiv.classList.add('d-none');
+            welcomeMessageDiv.classList.remove('d-none');
+            fireworks.start();
+            setTimeout(() => {
+                fireworks.stop();
+                window.location.href = 'http://unpaf.infinityfreeapp.com/';
+            }, 5000);
+        }
+    }, 1000);
+});
